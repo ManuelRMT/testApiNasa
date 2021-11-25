e4774712f1fc3bb2d140c06b546d296c0a950322
ManuelRMT <manuelruizmayat@gmail.com>
2021-11-25 19:32:35 +0100

    Delete src directory

--- src/main/java/com/test/atmira/backend/asteroids/demo/controller/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.test.atmira.backend.asteroids.demo.controller;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class Constant {
-
-	public static final ObjectMapper JSON_MAPPER = new ObjectMapper();
-}
